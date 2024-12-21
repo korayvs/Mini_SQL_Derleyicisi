0c559149760efa1bd076b7f83d4f8d1de9f7096b
Koray <Koray@DESKTOP-F1A12T8>
2024-12-21 19:22:59 +0300

    Add project files.

---
+++ Sorgu_Test/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Sorgu_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.CmbDataB = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CmbTablo = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(1, -1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(749, 382);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 387);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1023, 253);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SpringGreen;
+            this.button1.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(756, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Çalıştır";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button2.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(756, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(255, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Ekle - Sil - Güncelle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CmbDataB
+            // 
+            this.CmbDataB.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CmbDataB.FormattingEnabled = true;
+            this.CmbDataB.Location = new System.Drawing.Point(864, 46);
+            this.CmbDataB.Name = "CmbDataB";
+            this.CmbDataB.Size = new System.Drawing.Size(147, 26);
+            this.CmbDataB.TabIndex = 4;
+            this.CmbDataB.SelectedIndexChanged += new System.EventHandler(this.CmbDataB_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(762, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Veri Tabanı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(786, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tablolar:";
+            // 
+            // CmbTablo
+            // 
+            this.CmbTablo.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CmbTablo.FormattingEnabled = true;
+            this.CmbTablo.Location = new System.Drawing.Point(864, 78);
+            this.CmbTablo.Name = "CmbTablo";
+            this.CmbTablo.Size = new System.Drawing.Size(147, 26);
+            this.CmbTablo.TabIndex = 6;
+            this.CmbTablo.SelectedIndexChanged += new System.EventHandler(this.CmbTablo_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(1023, 640);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CmbTablo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CmbDataB);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox CmbDataB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CmbTablo;
+    }
+}
+
---
+++ Sorgu_Test/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Sorgu_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void DataB()
+        {
+            SqlConnection baglantiDB = new SqlConnection(@"Data Source=DESKTOP-F1A12T8\KORAY;Initial Catalog=master;Integrated Security=True");
+            baglantiDB.Open();
+            SqlCommand komut = new SqlCommand("Select name from sys.databases", baglantiDB);
+            SqlDataReader dr = komut.ExecuteReader();
+
+            while (dr.Read())
+            {
+                CmbDataB.Items.Add(dr[0].ToString());
+            }
+            baglantiDB.Close();
+        }
+
+        void Tbls()
+        {
+            SqlConnection baglantiTbl = new SqlConnection(@"Data Source=DESKTOP-F1A12T8\KORAY;Initial Catalog=" + CmbDataB.Text + ";Integrated Security=True");
+            baglantiTbl.Open();
+            SqlCommand komut1 = new SqlCommand("Select * From sys.Tables", baglantiTbl);
+            SqlDataReader dr1 = komut1.ExecuteReader();
+
+            while (dr1.Read())
+            {
+                CmbTablo.Items.Add(dr1[0].ToString());
+            }
+            baglantiTbl.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-F1A12T8\KORAY;Initial Catalog=" + CmbDataB.Text + ";Integrated Security=True");
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand(richTextBox1.Text, baglanti);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+
+                SqlDataAdapter da = new SqlDataAdapter("Select * From" + " " + CmbTablo.Text, baglanti);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Sorgunuzu Kontrol Edin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string sorgu = richTextBox1.Text;
+            //try
+            //{                
+            //    baglanti.Open();
+            //    SqlCommand komut = new SqlCommand(sorgu, baglanti);
+            //    komut.ExecuteNonQuery();
+            //    baglanti.Close();
+
+            //    SqlDataAdapter da = new SqlDataAdapter("Select * From TBLKISILER", baglanti);
+            //    DataTable dt = new DataTable();
+            //    da.Fill(dt);
+            //    dataGridView1.DataSource = dt;
+            //}
+            //catch (Exception)
+            //{
+            //    MessageBox.Show("Sorgunuzu Kontrol Edin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //} 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataB();
+            SqlConnection bgl = new SqlConnection(@"Data Source=DESKTOP-F1A12T8\KORAY;Initial Catalog=" + CmbDataB.Text + ";Integrated Security=True");
+        }
+
+        private void CmbDataB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CmbTablo.Items.Clear();
+            Tbls();
+        }
+
+        private void CmbTablo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Select * From" + " " + CmbTablo.Text;
+        }
+    }
+}
